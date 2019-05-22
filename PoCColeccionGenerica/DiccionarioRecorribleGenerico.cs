cb47a0bdd2d60eb6a05c3f6da00a7e268a26efeb
Sergio <elalonsou@hotmail.com>
2019-05-22 16:04:10 +0200

    Agregar archivos de proyecto.

---
+++ PoCColeccionGenerica/DiccionarioRecorribleGenerico.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoCColeccionGenerica
+{
+    class DiccionarioRecorribleGenerico<TKey, TValue> 
+    {
+        private Dictionary<TKey, TValue> _diccionario;
+        private List<TKey> _listaClaves;
+        public DiccionarioRecorribleGenerico()
+        {
+            _diccionario = new Dictionary<TKey, TValue>();
+            _listaClaves = new List<TKey>();
+        }
+           
+        private int _contPos = 0;
+
+
+        public void añadir(TKey clave, TValue valor)
+        {
+            _diccionario.Add(clave, valor);
+            _listaClaves.Add(clave);
+        }
+
+        public void MoverPrimero()
+        {
+            _contPos=0;
+        }
+
+        public int Cantidad()
+        {
+            return _diccionario.Count;
+        }
+
+        public bool EsUltimo()
+        {
+            if (_contPos + 1 >= _diccionario.Count || _diccionario.Count ==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void MoverSiguiente()
+        {
+            _contPos += 1;
+        }
+
+        public void MoverAnterior()
+        {
+            _contPos -= 1;
+        }
+
+        public TValue ObtenerActual()
+        {
+            return _diccionario[_listaClaves[_contPos]];
+        }
+
+        public TValue ObtenerValorPorClave(TKey clave)
+        {
+            return _diccionario[clave];
+        }
+    }
+}
---
+++ PoCColeccionGenerica/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PoCColeccionGenerica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiccionarioRecorribleGenerico<int, string> dicc = new DiccionarioRecorribleGenerico<int, string>();
+            dicc.añadir(1, "Hola 1");
+            dicc.añadir(2, "Hola 2");
+            dicc.añadir(3, "Hola 3");
+
+            bool fin = false;
+
+            if (dicc.Cantidad() != 0)
+            {
+                dicc.MoverPrimero();
+                while (!fin)
+                {
+                    Console.WriteLine($"Elemento Actual: {dicc.ObtenerActual()}");
+
+                    if (dicc.EsUltimo())
+                    {
+                        fin = true;
+                    }
+                    else
+                    {
+                        dicc.MoverSiguiente();
+                    }
+                }
+                Console.WriteLine($"Obteniendo elemento por clave 2: {dicc.ObtenerValorPorClave(2)}");
+                Console.WriteLine($"Obteniendo elemento por clave no existente: {dicc.ObtenerValorPorClave(6)}");
+                Console.WriteLine("Terminado ");
+            }
+        }
+    }
+}
